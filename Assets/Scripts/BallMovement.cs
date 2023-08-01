3cea695a8b68c44543852be8c4db5c59b5039222
Ayush Kumar Mishra <ayushmishra1376@gmail.com>
2023-08-01 23:52:10 +0530

    Level 3 Added

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -6,16 +6,43 @@ public class BallMovement : MonoBehaviour
 {
     public float speed;
     float xInput , zInput ;
+    public GameObject cam1, cam2, cam3, cam4;
 
     void Start()
     {
-        
+        Time.timeScale = 1f;
     }
 
     void Update()
     {
         xInput = Input.GetAxis("Horizontal");
         zInput = Input.GetAxis("Vertical");
-        transform.position = new Vector3(transform.position.x + xInput*speed, 0, transform.position.z + zInput*speed);
+        transform.position = new Vector3(transform.position.x + xInput*speed, transform.position.y, transform.position.z + zInput*speed);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            cam2.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                cam3.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    cam4.SetActive(true);
+                }
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            cam2.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                cam3.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    cam4.SetActive(true);
+                }
+            }
+        }
     }
 }
